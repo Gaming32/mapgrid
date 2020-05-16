769a7ad68e9c2f98acec8049c8329795441ca94b
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2020-05-16 08:46:54 -0500

    worked on gui a little

--- mapgrid.py
+++ mapgrid.py
@@ -1,7 +1,13 @@
+import os
 import tkinter
 
 
 matrix_labels = [None, 'up', 'down', 'left', 'right']
+
+
+def set_matrix_labels(labels):
+    global matrix_labels
+    matrix_labels = labels
 
 
 def create_matrix(width, height, default_value=matrix_labels[0]):
@@ -42,18 +48,18 @@ def unsquash_matrix(squashed, width, do_fillin=False, fillin_value=matrix_labels
 def save_matrix(matrix, fp):
     width = len(matrix[0])
     squashed = squash_matrix(matrix)
-    fp.write(width.to_bytes(2, 'big'))
+    fp.write(width.to_bytes(2, 'big', signed=False))
     last_element_count = 0
     for (ix, item) in enumerate(squashed):
         if ix == 0 or item == last_item:
             last_element_count += 1
         else:
-            fp.write(last_element_count.to_bytes(3, 'little'))
-            fp.write(matrix_labels.index(last_item).to_bytes(1, 'little'))
+            fp.write(last_element_count.to_bytes(3, 'little', signed=False))
+            fp.write(matrix_labels.index(last_item).to_bytes(1, 'little', signed=False))
             last_element_count = 1
         last_item = item
-    fp.write(last_element_count.to_bytes(3, 'little'))
-    fp.write(matrix_labels.index(last_item).to_bytes(1, 'little'))
+    fp.write(last_element_count.to_bytes(3, 'little', signed=False))
+    fp.write(matrix_labels.index(last_item).to_bytes(1, 'little', signed=False))
 
 
 def load_matrix(fp, do_fillin=False, fillin_value=matrix_labels[0]):
@@ -68,24 +74,84 @@ def load_matrix(fp, do_fillin=False, fillin_value=matrix_labels[0]):
     return unsquash_matrix(squashed, width, do_fillin, fillin_value)
 
 
+def embed(widget:tkinter.Widget, default_size=None, allow_new=True, allow_save=True, allow_load=True):
+    from tkinter import Label, Toplevel, Frame, IntVar, Spinbox, Button
+    from tkinter.messagebox import showerror
+    from PIL.ImageTk import PhotoImage
 
-if __name__ == '__main__':
-    from pprint import pp
+    matrix = []
+    buttons = []
 
-    matrix = create_matrix(5, 5)
-    matrix[1][2] = 'up'
-    matrix[3][2] = 'down'
-    matrix[2][1] = 'left'
-    matrix[2][3] = 'right'
-    print('matrix:')
-    pp(matrix)
-    print()
+    if default_size is None:
+        instructions = """n: New Map
+s: Save Map
+o/l: Open Map"""
+    else:
+        instructions = ''
+    instructions = Label(widget, text=instructions)
+    instructions.pack()
 
-    save_matrix(matrix, open('matrix.dat', 'wb'))
-    loaded = load_matrix(open('matrix.dat', 'rb'))
-    print('loaded:')
-    pp(loaded)
-    print()
+    def create_new_map(width, height):
+        matrix[:] = create_matrix(width, height)
+        instructions.destroy()
+        for old_button in buttons:
+            old_button.destroy()
+        buttons[:] = []
+        for x in range(width):
+            for y in range(height):
+                # new_button = Button(widget, photoimage=)
+                buttons.append()
 
-    print('result:', loaded == matrix)
+    if allow_new:
+        def new_map(event):
+            dialog = Toplevel(widget)
+            form = Frame(dialog)
+            Label(form, text='Width:').grid(row=0, column=0)
+            Label(form, text='Height:').grid(row=1, column=0)
+            size = [1, 1]
+            comm = (lambda ix: (lambda event=None: size.__setitem__(ix, wb.get() + (event.char if event else ''))))
+            wv = IntVar(value=10)
+            wb = Spinbox(form, from_=1, to=65535, command=comm(0), textvariable=wv)
+            wb.bind('<Key>', comm(0))
+            wb.grid(row=0, column=1)
+            hv = IntVar(value=10)
+            hb = Spinbox(form, from_=1, to=65535, command=comm(1), textvariable=hv)
+            hb.bind('<Key>', comm(1))
+            hb.grid(row=1, column=1)
+            form.pack()
+            dialog.transient(widget)
+            dialog.grab_set()
+            wb.focus_set()
+            widget.wait_window(dialog)
+            create_new_map(wv.get(), hv.get())
+        widget.bind('n', new_map)
+        widget.bind('N', new_map)
+
+    if allow_save:
+        def save_map(event):
+            ...
+        widget.bind('s', save_map)
+        widget.bind('S', save_map)
+
+    if allow_load:
+        def load_map(event):
+            ...
+        widget.bind('o', load_map)
+        widget.bind('l', load_map)
+        widget.bind('O', load_map)
+        widget.bind('L', load_map)
+
+    import glob
+    images = {x: PhotoImage(file=next(glob.iglob(os.path.join('img', str(x) + '*')))) for x in matrix_labels}
+    buttons = {'w': 'up', 'a': 'left', 's': 'down', 'd': 'right', '<space>': None}
+
+
+def main():
+    root = tkinter.Tk()
+    root.title('Map Grid')
+    embed(root)
+    root.mainloop()
+
+
+if __name__ == '__main__': main()
     
